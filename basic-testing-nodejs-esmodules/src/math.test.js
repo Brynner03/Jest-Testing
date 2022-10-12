180af8b984c77fba134c4ffbd9c7e45034f3e8ad
Brynner03 <bventura03@hotmail.com>
2022-10-12 06:57:15 -0400

    Installed dependencies, and added tests to other folders

---
+++ basic-testing-nodejs-esmodules/src/math.test.js
@@ -0,0 +1,77 @@
+import { it, expect } from 'vitest'
+
+import { add } from './math'
+
+it('Should summarize all number values in an array', () => {
+
+    // Arrange
+    const numbers = [1, 2, 3]
+    
+    // Act
+    const result = add(numbers) 
+    
+    // Assert
+    const expectedResult = numbers.reduce((prevValue, currValue) => prevValue + currValue, 0) 
+    expect(result).toBe(expectedResult)
+}) 
+
+it('Should yield NaN if at least one invalid number is provided', () => {
+    
+    // Arrange
+    const inputs = ['invalid', 1]
+
+    // Act 
+    const result = add(inputs)
+
+    // Assert  
+    expect(result).toBeNaN()
+})
+
+it('Should yield a correct sum if an array of numeric string values is provided', () => {
+    
+    // Arrange
+    const numbers = ['1', '2']
+
+    // Act
+    const result = add(numbers)
+
+    //Assert
+    const expectedResult = numbers.reduce((prev, curr) => +prev + +curr, 0)
+    expect(result).toBe(expectedResult)
+})
+
+it('Should yield 0 if an empty array is provided', () => {
+    // Arrange
+    const numbers = []
+
+    // Act
+    const result = add(numbers)
+
+    // Assert
+    expect(result).toBe(0)
+})
+
+it('Should throw an error if no value is passed into the function', () => {
+    
+    const resultFn = () => {
+        add()
+    }
+    // Assert
+    // Checks if resultFn throws an error.
+    expect(resultFn).toThrow()
+
+})
+
+it('Should throw an error if provided with multiple arguments instead of an array', () => {
+    
+    const num1 = 1
+    const num2 = 2
+
+    const resultFn = () => {
+        add(num1, num2)
+    }
+
+    // This looks for this specific error message
+    expect(resultFn).toThrow(/is not iterable/)
+
+})
---
+++ basic-testing-nodejs/src/util/validation.test.js
@@ -0,0 +1,75 @@
+import { it, expect, describe } from 'vitest'
+
+import { validateStringNotEmpty, validateNumber } from './validation'
+
+// Validate String
+describe('validateStringNotEmpty()', () => {
+
+    it('Should throw error if an empty string is provided', () => {
+        const input = ''
+        
+        const validationFn = () => validateStringNotEmpty(input)
+        
+        expect(validationFn).toThrow()
+    })
+    
+    it('Should throw an error with a message that contains a reason (must not be empty', () => {
+        const input = ''
+        const validationFn = () => validateStringNotEmpty(input)
+        expect(validationFn).toThrow(/must not be empty/)
+    })
+    
+    it('Should throw an error if a long string of blanks is provided', () => {
+        const input = ''
+        const validationFn = () => validateStringNotEmpty(input)
+        expect(validationFn).toThrow()
+    })
+    
+    it('Should throw an error if any other value than a string is provided.', () => {
+        const inputNum = 1
+        const inputBool = 1
+        const inputObj = {}
+        
+        const validationFnNum = () => validateStringNotEmpty(inputNum)
+        const validationFnBool = () => validateStringNotEmpty(inputBool)
+        const validationFnObj = () => validateStringNotEmpty(inputObj)
+        
+        expect(validationFnNum).toThrow()
+        expect(validationFnBool).toThrow()
+        expect(validationFnObj).toThrow()
+    })
+    
+    it('Should not throw an error if a non-empty string is provided.', () => {
+        const input = 'valid'
+        const validationFn = () => validateStringNotEmpty(input)
+        expect(validationFn).not.toThrow()
+    })
+})
+
+    // Validate Number
+describe('validateNumber()', () => {
+
+    it('Should throw an error if NaN is provided', () => {
+        const input = NaN
+        const validationFn = () => validateNumber(input)
+        expect(validationFn).toThrow()
+    })
+    
+    it('Should throw an error with a message that contains a reason (invalid number)', () => {
+        const input = NaN
+        const validationFn = () => validateNumber(input)
+        expect(validationFn).toThrow(/Invalid number input./)
+    })
+    
+    it('Should throw an error if a non-numeric value is provided', () => {
+        const input = '1' 
+        const validationFn = () => validateNumber(input)
+        expect(validationFn).toThrow()
+    })
+    
+    it('Should not throw an error if a number is provided', () => {
+        const input = 1
+        const validationFn = () => validateNumber(input)
+        expect(validationFn).not.toThrow()
+    })
+})
